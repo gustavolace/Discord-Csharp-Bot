81cdcf55ffd6ca6843f6d8ca66cb27170c1541fe
gustavolace <lsbgustavo@gmail.com>
2023-12-17 20:45:12 -0300

    Adicionar arquivos de projeto.

---
+++ Discord Bot/Program.cs
@@ -0,0 +1,23 @@
+﻿using DotNetEnv;
+using Discord.WebSocket;
+
+
+        Env.Load();
+        var token = Environment.GetEnvironmentVariable("DiscordToken");
+        var client = new DiscordSocketClient();
+
+        await client.LoginAsync(Discord.TokenType.Bot, token);
+
+        await client.StartAsync();
+
+        client.Ready += async () =>
+        {
+        var guild = client.GetGuild(896940870341890059);
+        var channel = guild.GetTextChannel(936372639822401541);
+
+        await channel.SendMessageAsync("Ola boa noite");
+        };
+
+
+        Console.ReadKey();
+
